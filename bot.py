5aa76592b2b17f21b0878a3e4def92dc6ee52665
AvinashValuveri <73448603+AvinashValuveri@users.noreply.github.com>
2021-03-15 12:59:15 -0400

    Added roles reaction and welcome message
    When user joins server, it will send a welcome message, also added a roles reaction feature which sends dm confirming wheter you got the role or not.

--- bot.py
+++ bot.py
@@ -27,19 +27,102 @@ def delete_quote(index):
 
 
 
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix = '!')
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
+@client.event
+async def on_member_join(member):
+    await member.send("Hello , please answer the questions from #verification-questions to have access to the whole sever! Also make sure to react to roles in #roles to get your roles!")
 
+@client.event
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    if message_id == 820995434083385364:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
 
+        if payload.emoji.name == 'rightwingauthoritarian':
+            role = discord.utils.get(guild.roles, name='Right Wing Authoritarian')
+        elif payload.emoji == 'leftistauthoritarian':
+            role = discord.utils.get(guild.roles, name='Leftist Authoritarian')
+        elif payload.emoji.name == 'rightwinglibertarian':
+            role = discord.utils.get(guild.roles, name='Right Wing Libertarian')
+        elif payload.emoji.name == 'socdem':
+          role = discord.utils.get(guild.roles, name='Social Democrat')
+        elif payload.emoji.name == 'communism':
+          role = discord.utils.get(guild.roles, name='Socialist/Communist')
+        elif payload.emoji.name == 'conservative':
+          role = discord.utils.get(guild.roles, name='Conservative')
+        elif payload.emoji.name == 'liberal':
+          role = discord.utils.get(guild.roles, name='Liberal')
+        elif payload.emoji.name == 'leftlibertarian':
+          role = discord.utils.get(guild.roles, name='Leftist Libertarian')
+        elif payload.emoji.name == 'leftistauthoritarian':
+          role = discord.utils.get(guild.roles, name='Leftist Authoritarian')
+        elif payload.emoji.name == 'centrist':
+          role = discord.utils.get(guild.roles, name='Centrist')         
+        elif payload.emoji.name == 'fascist':
+          role = discord.utils.get(guild.roles, name='Fascist')  
+        else:
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
+        
+        if role is not None:
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.add_roles(role)
+                print('Role added')
+                await member.send(f"{role} role successfully added")
 
-
+            else:
+                print("Member not found")
 
 
 @client.event
+async def on_raw_reaction_remove(payload):
+    message_id = payload.message_id
+    if message_id == MESSAGE_ID:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        if payload.emoji.name == 'rightwingauthoritarian':
+            role = discord.utils.get(guild.roles, name='Right Wing Authoritarian')
+        elif payload.emoji == 'leftistauthoritarian':
+            role = discord.utils.get(guild.roles, name='Leftist Authoritarian')
+        elif payload.emoji.name == 'rightwinglibertarian':
+            role = discord.utils.get(guild.roles, name='Right Wing Libertarian')
+        elif payload.emoji.name == 'socdem':
+          role = discord.utils.get(guild.roles, name='Social Democrat')
+        elif payload.emoji.name == 'communism':
+          role = discord.utils.get(guild.roles, name='Socialist/Communist')
+        elif payload.emoji.name == 'conservative':
+          role = discord.utils.get(guild.roles, name='Conservative')
+        elif payload.emoji.name == 'liberal':
+          role = discord.utils.get(guild.roles, name='Liberal')
+        elif payload.emoji.name == 'leftlibertarian':
+          role = discord.utils.get(guild.roles, name='Leftist Libertarian')
+        elif payload.emoji.name == 'centrist':
+          role = discord.utils.get(guild.roles, name='Centrist')         
+        elif payload.emoji.name == 'fascist':
+          role = discord.utils.get(guild.roles, name='Fascist')  
+        else:
+            role = discord.utils.get(guild.roles, name=payload.emoji.name)
+        
+        if role is not None:
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            if member is not None:
+                await member.remove_roles(role)
+                print('Role removed')
+                await member.send(f"{role} role successfully removed")
+            else:
+                print("Member not found")
+
+@client.event
 async def on_ready():
+  activity = discord.Game(name="$help | https://discord.gg/6PSyQ5nwGf")
+  await client.change_presence(status=discord.Status.online, activity=activity)
   print('We have logged in as {0.user}'.format(client))
-
-
 
 
 
@@ -53,16 +136,23 @@ async def on_message(message):
   if "quotes" in db.keys():
     options = options + db["quotes"]
   
+  if message.content.startswith('$help'):
+    await message.channel.send('''
+    $help - for list of commands
+    !quote - use this command so the bot can send a quote belonging to a famoous political leader and falsely attribute it to a rapper
+    !newquote - use this command to add a new quote to the database 
+    !delquote - use this command to delete a quote in the database by referencing its index like this: !delquote *index*
+    !list - check list of quotes added by users, not including list added by the dev
+    ''')
   if message.content.startswith(('!quote')):
     await message.channel.send(random.choice(options))
   if message.content.startswith(('!quote')):
     await message.channel.send(random.choice(rapper_names))
   if message.content.startswith('Vaush'):
-    await message.channel.send('Lol Vaushite')
+    await message.channel.send('Lol Vaush')
   if message.content.startswith('!hello'):
     await message.channel.send('Hello!')
 
-  
   
   if message.content.startswith("!newquote"):
     quotes_message = message.content.split("!newquote ",1)[1]
@@ -85,4 +175,4 @@ async def on_message(message):
 
 
 keep_alive()
-client.run(TOKEN)
+client.run('TOKEN')
